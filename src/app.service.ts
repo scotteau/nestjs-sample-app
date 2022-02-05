3fae82bbeaf18cfe6f24a322e170ef01c8135652
Scott Wang <scotteau@gmail.com>
2022-02-05 16:14:02 +0800

    initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getLove(): string {
+    return 'Love Love Love!';
+  }
+}
